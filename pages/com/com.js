458fe5fa21ac82ad42c5fa5bf8b4810464900e5c
tww <710752055@qq.com>
2018-07-26 10:34:27 +0800

    update 表单  label

--- pages/com/com.js
+++ pages/com/com.js
@@ -27,14 +27,122 @@ var pageObject = {
     disabled: false,
     plain: false,
     loading: false,
-    items: [
+    items: [{
+        name: 'USA',
+        value: '美国'
+      },
+      {
+        name: 'CHN',
+        value: '中国',
+        checked: 'true'
+      },
+      {
+        name: 'BRA',
+        value: '巴西'
+      },
+      {
+        name: 'JPN',
+        value: '日本'
+      },
+      {
+        name: 'ENG',
+        value: '英国'
+      },
+      {
+        name: 'TUR',
+        value: '法国'
+      },
+    ],
+    focus: false,
+    inputValue: '',
+    checkboxItems: [
+      { name: 'USA', value: '美国' },
+      { name: 'CHN', value: '中国', checked: 'true' },
+      { name: 'BRA', value: '巴西' },
+      { name: 'JPN', value: '日本', checked: 'true' },
+      { name: 'ENG', value: '英国' },
+      { name: 'TUR', value: '法国' },
+    ],
+    radioItems: [
       { name: 'USA', value: '美国' },
       { name: 'CHN', value: '中国', checked: 'true' },
       { name: 'BRA', value: '巴西' },
       { name: 'JPN', value: '日本' },
       { name: 'ENG', value: '英国' },
       { name: 'TUR', value: '法国' },
-    ]
+    ],
+    hidden: false
+  },
+
+
+
+  checkboxChange: function (e) {
+    var checked = e.detail.value
+    var changed = {}
+    for (var i = 0; i < this.data.checkboxItems.length; i++) {
+      if (checked.indexOf(this.data.checkboxItems[i].name) !== -1) {
+        changed['checkboxItems[' + i + '].checked'] = true
+      } else {
+        changed['checkboxItems[' + i + '].checked'] = false
+      }
+    }
+    this.setData(changed)
+  },
+  radioChange:function(e){
+    var checked = e.detail.value
+    var changed = {}
+    for (var i = 0; i < this.data.radioItems.length; i++) {
+      if (checked.indexOf(this.data.radioItems[i].name) !== -1) {
+        changed['radioItems[' + i + '].checked'] = true
+      } else {
+        changed['radioItems[' + i + '].checked'] = false
+      }
+    }
+    this.setData(changed)
+  },
+  bindButtonTap: function(e) {
+    this.setData({
+      focus: true
+    })
+  },
+  bindKeyInput: function(e) {
+    this.setData({
+      inputValue: e.detail.value
+    })
+  },
+  bindReplaceInput: function(e) {
+    var value = e.detail.value
+    var pos = e.detail.cursor
+    var left
+    if (pos !== -1) {
+      // 光标在中间
+      left = e.detail.value.slice(0, pos)
+      // 计算光标的位置
+      pos = left.replace(/11/g, '2').length
+    }
+
+    // 直接返回对象，可以对输入进行过滤处理，同时可以控制光标的位置
+    return {
+      value: value.replace(/11/g, '2'),
+      cursor: pos
+    }
+
+    // 或者直接返回字符串,光标在最后边
+    // return value.replace(/11/g,'2'),
+  },
+  bindHideKeyboard: function(e) {
+    if (e.detail.value === '123') {
+      // 收起键盘
+      wx.hideKeyboard()
+    }
+  },
+
+
+  formSubmit: function(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  formReset: function() {
+    console.log('form发生了reset事件')
   },
 
   add: function(e) {
@@ -54,10 +162,10 @@ var pageObject = {
     }
 
   },
-  checkboxChange: function (e) {
+  checkboxChange: function(e) {
     console.log('checkbox发生change事件，携带value值为：', e.detail.value)
   },
-  
+
   default: function(e) {
 
   },
